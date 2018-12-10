fe0c9bba9fa497da47b666dd3d85bf02752be1ab
aurelien <aurelien.rausch@etu.unistra.fr>
2018-12-10 15:40:30 +0100

    add kernel 3

--- gpu-op.cu
+++ gpu-op.cu
@@ -137,6 +137,29 @@ __global__ void MatrixProductKernel_v2(void)
   }
 }
 
+__global__ void MatrixProductKernel_v3(void)
+{
+  // Index computations
+  int lig = threadIdx.y + blockIdx.y * BLOCK_SIZE_XY_K3;
+  int col = threadIdx.x + blockIdx.x * BLOCK_SIZE_XY_K3;
+  __shared__ T_real data_A[BLOCK_SIZE_XY_K3][BLOCK_SIZE_XY_K3];
+  __shared__ T_real data_B[BLOCK_SIZE_XY_K3][BLOCK_SIZE_XY_K3];
+  T_real res = .0;
+
+  for (int e = 0; e < SIZE / BLOCK_SIZE_XY_K3; e++) {
+    data_A[threadIdx.y][threadIdx.x] = GPU_A[lig][e * BLOCK_SIZE_XY_K3 + threadIdx.x];
+    data_B[threadIdx.y][threadIdx.x] = GPU_B[e * BLOCK_SIZE_XY_K3 + threadIdx.y][col];
+    __syncthreads();
+    // Matrix product computation
+    for (int i = 0; i < BLOCK_SIZE_XY_K3; i++) {
+      res += data_A[threadIdx.y][i] * data_B[i][threadIdx.x];
+    }
+    __syncthreads();
+  }
+
+  GPU_C[lig][col] = res;
+}
+
 
 /*-------------------------------------------------------------------------------*/
 /* Small matrix product on the local GPU.                                        */
@@ -168,10 +191,10 @@ void gpuProduct(gkid_t kid)
    Dg.z = 1;
    // - run the Grid of Blocs of threads
    MatrixProductKernel_v1<<<Dg,Db>>>();
-      break;
+   break;
 
  case GK2 :
-// - init the grid of blocs
+   // - init the grid of blocs
    Db.x = BLOCK_SIZE_X_K1;
    Db.y = BLOCK_SIZE_Y_K1;
    Db.z = 1;
@@ -180,10 +203,19 @@ void gpuProduct(gkid_t kid)
    Dg.z = 1;
    // - run the Grid of Blocs of threads
    MatrixProductKernel_v2<<<Dg,Db>>>();
-  break;
+   break;
 
  case GK3 :
-  break;
+   // - init the grid of blocs
+   Db.x = BLOCK_SIZE_XY_K3;
+   Db.y = BLOCK_SIZE_XY_K3;
+   Db.z = 1;
+   Dg.x = SIZE / BLOCK_SIZE_XY_K3; 
+   Dg.y = SIZE / BLOCK_SIZE_XY_K3;
+   Dg.z = 1;
+   // - run the Grid of Blocs of threads
+   MatrixProductKernel_v3<<<Dg,Db>>>();
+   break;
 
  case GK4 :
   break;
